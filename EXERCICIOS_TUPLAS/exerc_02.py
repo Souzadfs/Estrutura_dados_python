d28f631cc362e6aaefd1a408f72a227d8a60671a
Souzadfs <souzadf09@gmail.com>
2024-03-23 20:00:52 -0300

    tuplas

---
+++ EXERCICIOS_TUPLAS/exerc_02.py
@@ -0,0 +1,8 @@
+camp = ('Palmeiras','Internacional','Santos','Coritiba','São Paulo','Flamengo','Corintias','Atletico PR','Cruzeiro','Sport','Bragantino','Botafogo','Vasco','Atletico MG','Ferroviaria',)
+
+
+print(camp[0:4])
+print(camp [-4:])
+print(len(camp))
+
+
