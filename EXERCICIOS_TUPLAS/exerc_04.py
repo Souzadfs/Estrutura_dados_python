24d9b441c0e97ea79e7f0da8d32821724db4f3fd
Souzadfs <souzadf09@gmail.com>
2024-04-04 21:32:26 -0300

    Exercicios

--- EXERCICIOS_TUPLAS/exerc_04.py
+++ EXERCICIOS_TUPLAS/exerc_04.py
@@ -5,5 +5,11 @@ num =  (
     int(input('Digite o ultimo número: ')),
 )
 print(num)
+print(f'O número 9 apareceu {num.count(9)} x')
+print(f'O número 3 aprececeu na posição {num.index(3)+1}°')
+
+for i in num:
+    if i % 2 == 0:
+        print(f'O número par é {i} e esta na posição {num.index(i)+1}')
 
 
